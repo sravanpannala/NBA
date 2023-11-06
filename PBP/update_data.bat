82bc67bcd08fa91aaecc30e2742fb33840420d6e
Sravan Pannala <57152030+sravanpannala@users.noreply.github.com>
2023-11-06 13:39:51 -0500

    add RAPM code, add nba player json code

--- PBP/update_data.bat
+++ PBP/update_data.bat
@@ -1,2 +1,2 @@
-C:\Users\pansr\AppData\Local\pypoetry\Cache\virtualenvs\nba-dIF8YUVF-py3.9\Scripts\python.exe "C:\Users\pansr\Documents\Sra_Coding\NBA\PBP\update_data.py"
+C:\Users\pansr\Documents\Sra_Coding\NBA\.venv\Scripts\python.exe "C:\Users\pansr\Documents\Sra_Coding\NBA\PBP\update_data.py"
 pause
