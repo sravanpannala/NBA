aa913239be5475991b5f833cb24159b2c6d1894d
Sravan Pannala <57152030+sravanpannala@users.noreply.github.com>
2023-11-06 12:48:49 -0500

    update shot charts code

--- PBP/update_data.py
+++ PBP/update_data.py
@@ -4,9 +4,10 @@ from tqdm import tqdm
 from pbpstats.client import Client
 
 pbp_DIR = "C:/Users/pansr/Documents/Sra_Coding/NBA/pbpdata"
+data_provider = "data_nba"
 
 settings = {
-    "Games": {"source": "web", "data_provider": "data_nba"},
+    "Games": {"source": "web", "data_provider": data_provider},
      "dir": pbp_DIR,
 }
 client = Client(settings)
@@ -18,10 +19,13 @@ client = Client(settings)
 seasons = ["2019","2020","2021","2022","2023"]
 season_types = ["Regular Season","Playoffs"]
 leagues = ["nba","wnba"]
+seasons = ["2023"]
+season_types = ["Regular Season"]
+leagues = ["nba"]
 for season_yr in seasons:
     for league in leagues:
         for season_type in season_types:
-            print(league)
+            print(f"{season_yr},{league},{season_type}")
             season = client.Season(league, season_yr, season_type)
             games_id = []
             k = 0
@@ -31,8 +35,8 @@ for season_yr in seasons:
 
             print('Number of games: ',len(games_id))
             settings = {
-                "Boxscore": {"source": "file", "data_provider": "data_nba"},
-                "Possessions": {"source": "file", "data_provider": "data_nba"},
+                "Boxscore": {"source": "file", "data_provider": data_provider},
+                "Possessions": {"source": "file", "data_provider": data_provider},
                 "dir": pbp_DIR,
             }
             client = Client(settings)
@@ -57,8 +61,8 @@ for season_yr in seasons:
             print('Number of missing games: ',len(games_list_online))
 
             settings = {
-                "Boxscore": {"source": "web", "data_provider": "data_nba"},
-                "Possessions": {"source": "web", "data_provider": "data_nba"},
+                "Boxscore": {"source": "web", "data_provider": data_provider},
+                "Possessions": {"source": "web", "data_provider": data_provider},
                 "dir": pbp_DIR,
             }
             client = Client(settings)
@@ -68,12 +72,12 @@ for season_yr in seasons:
                 try:
                     client.Game(gameid)
                 except Exception as error:
-                    print(error)
+                    # print(error)
                     # error_list.append(error.args[0])
                     continue
             # print(error_list)
-            with open('output.txt', 'a') as f:
-                f.write("\n")
-                f.write(f"{season},{league},{season_type}")
-                f.write("\n")
-                f.write(error_list)
+            # with open('output.txt', 'a') as f:
+            #     f.write("\n")
+            #     f.write(f"{season},{league},{season_type}")
+            #     f.write("\n")
+            #     f.write(error_list)
--- Shot_Charts/README.md
+++ Shot_Charts/README.md
@@ -1,8 +1,6 @@
 # Plot Shot Charts
-
 - Get location data using PBP Stats API
-- Plot location heat maps for NBA
-- Plot location heat maps for WNBA (probably need to merge both files)
+- Plot location heat maps for NBA and WNBA
 - Plot shot hex map using NBA API and plotly
 
 
