d64c649d4877e89c881183c65583abfb0e1c2906
Sravan Pannala <57152030+sravanpannala@users.noreply.github.com>
2023-12-17 20:34:46 -0500

    add scraping for injury, DARKO and NBA games missed analysis.

--- R/bbref_save_data.r
+++ R/bbref_save_data.r
@@ -25,25 +25,25 @@ df1 <- bref_players_stats(
   return_message = TRUE
 )
 
-path <- paste0("./player_analysis/fdata/NBA_bbref_Player_Adv_",season-1,".csv")
+path <- paste0("./fdata/NBA_bbref_Player_Adv_",season-1,".csv")
 write.csv(df1, path)
 
 ## Multiple Seasons
 
-for (season in 2001:2024) {
-  df1 <- bref_players_stats(
-    seasons = season,
-    tables = c("advanced","per_game"),
-    include_all_nba = FALSE,
-    only_totals = FALSE,
-    nest_data = FALSE,
-    assign_to_environment = TRUE,
-    widen_data = TRUE,
-    join_data = TRUE,
-    return_message = TRUE
-  )
+# for (season in 2001:2024) {
+#   df1 <- bref_players_stats(
+#     seasons = season,
+#     tables = c("advanced","per_game"),
+#     include_all_nba = FALSE,
+#     only_totals = FALSE,
+#     nest_data = FALSE,
+#     assign_to_environment = TRUE,
+#     widen_data = TRUE,
+#     join_data = TRUE,
+#     return_message = TRUE
+#   )
 
-  # path <- paste0("./player_analysis/fdata/NBA_bbref_Player_Adv_",season-1,".csv")
-  # write.csv(df1, path)
+#   # path <- paste0("./player_analysis/fdata/NBA_bbref_Player_Adv_",season-1,".csv")
+#   # write.csv(df1, path)
 
-}
+# }
